e156c93c4fb4f8c1af26759482b70e618f008c43
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-09-14 17:14:42 +0200

    try new deduplication strategy

--- src/server/apollo/dataSources.ts
+++ src/server/apollo/dataSources.ts
@@ -52,6 +52,14 @@ export class CustomAPI extends RESTDataSource {
   }
 
   /**
+   * Try to override deduplicate
+   * @returns deduplicate policy
+   */
+  protected override requestDeduplicationPolicyFor() {
+    return { policy: 'do-not-deduplicate' } as const;
+  }
+
+  /**
    * Pass auth token if needed.
    *
    * @param _ path, not used.
